39471380736f90838e8862da4e0fa1c8a0a512a2
tauri_s <tauristclaire@gmail.com>
2026-09-23 12:48:30 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var tempCelsius = 10;
+var toFahrenheit = tempCelsius * 1.8 + 32;
+
+console.log(
+  `Outside my friend's house it's ${tempCelsius} degrees Celsius, which is ${toFahrenheit} degrees Fahrenheit.`
+);
